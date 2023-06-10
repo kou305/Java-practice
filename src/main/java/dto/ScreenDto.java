56130629b667c732755c9d36f5204f95724913e3
kou305 <srenkon788@gmail.com>
2023-06-10 23:35:48 +0900

    [update] Bean更新

--- src/main/java/dto/ScreenDto.java
+++ src/main/java/dto/ScreenDto.java
@@ -3,5 +3,16 @@ package dto;
 import java.io.Serializable;
 
 public class ScreenDto implements Serializable {
-
+	/* 曲名 */
+	private String songTitle;
+	/* アルバム名 */
+	private String albumTitle;
+	/* アーティスト名 */
+	private String artinst;
+	/* 発売年 */
+	private String releaseYear;
+	/* ジャンル */
+	private String genre;
+	/* 感想 */
+	private String thought;
 }
--- src/main/java/propBean/PropartyDataBean.java
+++ src/main/java/propBean/PropartyDataBean.java
@@ -3,14 +3,16 @@ package propBean;
 import java.io.Serializable;
 
 public class PropartyDataBean implements Serializable {
-	/*  */
+	/* DBURL */
 	private String DBURL;
-	/*  */
+	/* DBユーザ */
 	private String DBUser;
-	/*  */
+	/* DBパスワード */
 	private String DBPass;
-	/*  */
+	/* 正常画面用のテンプレートHTML配置パス */
 	private String templateHTMLPath;
+	/* エラー画面用のテンプレートHTML配置パス */
+	private String templateErrorHTMLPath;
 	
 	public String getDBURL() {
 		return DBURL;
@@ -35,6 +37,12 @@ public class PropartyDataBean implements Serializable {
 	}
 	public void setTemplateHTMLPath(String templateHTMLPath) {
 		this.templateHTMLPath = templateHTMLPath;
+	}
+	public String getTemplateErrorHTMLPath() {
+		return templateErrorHTMLPath;
+	}
+	public void setTemplateErrorHTMLPath(String templateErrorHTMLPath) {
+		this.templateErrorHTMLPath = templateErrorHTMLPath;
 	}
 	
 }
